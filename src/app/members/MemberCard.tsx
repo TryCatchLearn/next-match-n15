74ec0e9f471672ba4979a6ca5b0f8b821145aff0
Neil Cummings <trycatchlearn@outlook.com>
2025-02-07 19:02:58 +0700

    End of section 5

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,14 +1,26 @@
+'use client';
+
 import {Member} from '@prisma/client';
 import {Card, CardFooter} from '@heroui/card';
 import {Image} from '@heroui/image';
 import Link from 'next/link';
 import {calculateAge} from '@/lib/util';
+import LikeButton from '@/components/LikeButton';
+import React from 'react';
 
 type Props = {
     member: Member;
+    likeIds: string[];
 }
 
-export default function MemberCard({member}: Props) {
+export default function MemberCard({member, likeIds}: Props) {
+    const hasLiked = likeIds.includes(member.userId);
+
+    const preventLinkAction = (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
     return (
         <Card
             fullWidth
@@ -23,6 +35,12 @@ export default function MemberCard({member}: Props) {
                 src={member.image || '/images/user.png'}
                 className='aspect-square object-cover'
             />
+            <div onClick={preventLinkAction}>
+                <div className='absolute top-3 right-3 z-50'>
+                    <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+                </div>
+            </div>
+
             <CardFooter
                 className='flex justify-start bg-dark-gradient overflow-hidden absolute bottom-0 z-10'>
                 <div className='flex flex-col text-white'>
