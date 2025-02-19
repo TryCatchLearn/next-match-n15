3cb42094f03c7268beacdc46d8d86a17c1988893
Neil Cummings <trycatchlearn@outlook.com>
2025-02-19 14:27:53 +0700

    End of section 12

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -6,8 +6,9 @@ import {Image} from '@heroui/image';
 import Link from 'next/link';
 import {calculateAge, transformImageUrl} from '@/lib/util';
 import LikeButton from '@/components/LikeButton';
-import React from 'react';
+import React, { useState } from 'react';
 import PresenceDot from '@/components/PresenceDot';
+import { toggleLikeMember } from '../actions/likeActions';
 
 type Props = {
     member: Member;
@@ -15,7 +16,20 @@ type Props = {
 }
 
 export default function MemberCard({member, likeIds}: Props) {
-    const hasLiked = likeIds.includes(member.userId);
+    const [hasLiked, setHasLiked] = useState(likeIds.includes(member.userId));
+    const [loading, setLoading] = useState(false);
+
+    async function toggleLike() {
+        setLoading(true);
+        try {
+            await toggleLikeMember(member.userId, hasLiked);
+            setHasLiked(!hasLiked);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     const preventLinkAction = (e: React.MouseEvent) => {
         e.preventDefault();
@@ -38,7 +52,7 @@ export default function MemberCard({member, likeIds}: Props) {
             />
             <div onClick={preventLinkAction}>
                 <div className='absolute top-3 right-3 z-50'>
-                    <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+                    <LikeButton loading={loading} toggleLike={toggleLike} hasLiked={hasLiked} />
                 </div>
                 <div className='absolute top-2 left-3 z-50'>
                     <PresenceDot member={member} />
