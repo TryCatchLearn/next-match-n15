71e8f98f7d2504527aedf06f32c7edd158ff8557
Neil Cummings <trycatchlearn@outlook.com>
2025-02-19 09:18:16 +0700

    End of section 11

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,12 +5,13 @@ import useMessageStore from "@/hooks/useMessageStore";
 import { useNotificationChannel } from "@/hooks/useNotificationChannel";
 import { usePresenceChannel } from "@/hooks/usePresenceChannel";
 import { HeroUIProvider } from "@heroui/react";
+import { SessionProvider } from "next-auth/react";
 import { ReactNode, useCallback, useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-export default function Providers({children, userId, profileComplete}: 
-  {children: ReactNode, userId: string | null, profileComplete: boolean}) {
+export default function Providers({ children, userId, profileComplete }:
+  { children: ReactNode, userId: string | null, profileComplete: boolean }) {
   const updateUnreadCount = useMessageStore(state => state.updateUnreadCount);
 
   const setUnreadCount = useCallback((amount: number) => {
@@ -24,13 +25,16 @@ export default function Providers({children, userId, profileComplete}:
       })
     }
   }, [setUnreadCount, userId]);
-  
+
   usePresenceChannel(userId, profileComplete);
   useNotificationChannel(userId, profileComplete);
   return (
-    <HeroUIProvider>
-      <ToastContainer position="bottom-right" hideProgressBar className='z-50' />
+    <SessionProvider>
+      <HeroUIProvider>
+        <ToastContainer position="bottom-right" hideProgressBar className='z-50' />
         {children}
-    </HeroUIProvider>
+      </HeroUIProvider>
+    </SessionProvider>
+
   )
 }
